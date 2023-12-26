55602b49a5892ea0ff7079d467ae0cba9a26d8b8
lsdev200 <lsdev200@gmail.com>
2023-12-26 12:54:37 +0530

    added new keys

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1ORCDWSByGkL98l59pLVPqZg' },
+          { shipping_rate: 'shr_1ORUtcSJoXkDxgiMrUE87Xu8' },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
