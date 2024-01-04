7a34d8b0e7b64895d1d432d83bfd40c7111d054e
lsdev200 <lsdev200@gmail.com>
2024-01-04 12:43:52 +0530

    hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import { Layout } from '../components'
-import '../styles/globals.css'
 import { StateContext } from '../context/StateContext'
 import { Toaster } from "react-hot-toast"
+import '../styles/globals.css'
+import Hotjar from '@hotjar/browser';
+
+const siteId = 3812917;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
+
 
 function MyApp({ Component, pageProps }) {
+  
+
   return (
     <StateContext>
       <Toaster />
